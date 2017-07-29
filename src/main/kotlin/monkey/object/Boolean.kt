b609edefff984f65c77437286b11f4872b605589
andrea <vanpivix@gmail.com>
2017-07-29 18:17:56 +0200

    Fix Boolean type

--- src/main/kotlin/monkey/object/Boolean.kt
+++ src/main/kotlin/monkey/object/Boolean.kt
@@ -4,7 +4,7 @@ package monkey.`object`
  * @author andrea
  * @since 7/29/17
  */
-class Boolean(val value: Boolean) : Object(ObjectType.BOOLEAN) {
+class Boolean(val value: kotlin.Boolean) : Object(ObjectType.BOOLEAN) {
 
     override fun inspect() = value.toString()
 }
