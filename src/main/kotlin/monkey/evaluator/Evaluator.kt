940f57fcb7096d84b13e3880d96d317e575093a0
andrea <vanpivix@gmail.com>
2017-07-29 18:07:18 +0200

    Eval integer literal expressions

---
+++ src/main/kotlin/monkey/evaluator/Evaluator.kt
@@ -0,0 +1,31 @@
+package monkey.evaluator
+
+import monkey.`object`.Integer
+import monkey.`object`.Null
+import monkey.`object`.Object
+import monkey.ast.Node
+import monkey.ast.Program
+import monkey.ast.expressions.IntegerLiteralExpression
+import monkey.ast.statements.ExpressionStatement
+import monkey.ast.statements.Statement
+
+/**
+ * @author andrea
+ * @since 7/29/17
+ */
+object Evaluator {
+
+    fun eval(node: Node): Object = when (node::class) {
+        Program::class -> evalStatements((node as Program).statements)
+        ExpressionStatement::class -> eval((node as ExpressionStatement).expression)
+        IntegerLiteralExpression::class -> Integer((node as IntegerLiteralExpression).value)
+        else -> throw RuntimeException("Unknown node implementation ${node.javaClass}")
+    }
+
+    private fun evalStatements(statements: List<Statement>): Object {
+        var result: Object = Null()
+        // TODO why
+        statements.forEach { result = eval(it) }
+        return result
+    }
+}
---
+++ src/test/kotlin/monkey/evaluator/Evaluator.kt
@@ -0,0 +1,33 @@
+package monkey.evaluator
+
+import com.winterbe.expekt.should
+import monkey.`object`.Integer
+import monkey.`object`.Object
+import monkey.ast.Parser
+import monkey.lexer.StringLexer
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+
+/**
+ * @author andrea
+ * @since 7/29/17
+ */
+object EvaluatorTest : Spek({
+    describe("evaluator") {
+        it("Test evaluation of integer expressions") {
+            mapOf("5" to 5, "10" to 10)
+                    .forEach { testIntegerObject(testEval(it.key), it.value) }
+        }
+    }
+})
+
+fun testEval(input: String): Object {
+    val program = Parser(StringLexer(input)).parseProgram()
+    return Evaluator.eval(program)
+}
+
+fun testIntegerObject(obj: Object, expected: Int) {
+    obj.should.be.instanceof(Integer::class.java)
+    (obj as Integer).value.should.be.equal(expected)
+}
