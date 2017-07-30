1b2c72c67b53d56037116fc4bbcbca41dae77f31
andrea <vanpivix@gmail.com>
2017-07-30 22:15:21 +0200

    Run monkey scripts

--- monkey
+++ monkey
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-./gradlew --console plain run -q
+if [ $# -eq 0 ]; then
+    ./gradlew --console plain run -q
+else
+    ./gradlew --console plain run -q -PappArgs="['${1}']"
+fi
--- src/main/kotlin/monkey/Main.kt
+++ src/main/kotlin/monkey/Main.kt
@@ -1,11 +1,25 @@
 package monkey
 
+import com.andreapivetta.kolor.red
+import monkey.`object`.Environment
+import monkey.ast.Parser
+import monkey.evaluator.Evaluator
+import monkey.lexer.StringLexer
 import monkey.repl.REPL
+import java.io.File
 
 /**
  * @author andrea
  * @since 7/18/17
  */
 fun main(args: Array<String>) {
-    REPL.start()
+    if (args.size == 1) {
+        val input = File(args[0])
+        if (input.exists()) {
+            val code = input.inputStream().bufferedReader().use { it.readText() }
+            Evaluator.eval(Parser(StringLexer(code)).parseProgram(), Environment())
+        } else println("File not found: ${args[0]}".red())
+    } else {
+        REPL.start()
+    }
 }
